e807244a615a7291a712e5411fbe047f4c163b77
Mark Topper <mail@webman.io>
2015-12-11 09:29:28 +0100

    Create Facade.php

---
+++ src/Larapack/ConfigWriter/Facade.php
@@ -0,0 +1,25 @@
+<?php
+	
+namespace Larapack\ConfigWriter;
+
+use Illuminate\Support\Facades\Config;
+
+class Facade extends Config
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public static function write($config, array $newValues = [], $validate = true)
+    {
+        $config = new Repository($config);
+	    
+	    foreach ($newValues AS $key => $value)
+	    {
+		    $config->set($key, $value);
+	    }
+	    
+	    $config->save(null, null, $validate);
+    }
+}
